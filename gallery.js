5be2189f23f46b413f94a0cfe05790dd540e8ec3
rodrosn0w <rodronieva1@gmail.com>
2024-06-09 22:44:42 -0300

    Falopa de las fotos

---
+++ gallery.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    var currentIndex = 0;
+    var images = $('.gallery-item');
+
+    function openLightbox(index) {
+        currentIndex = index;
+        $('#lightbox').css('display', 'block');
+        $('.lightbox-content').attr('src', images.eq(currentIndex).attr('src'));
+        $('.caption').text(images.eq(currentIndex).attr('alt'));
+    }
+
+    function closeLightbox() {
+        $('#lightbox').css('display', 'none');
+    }
+
+    function showPrev() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        openLightbox(currentIndex);
+    }
+
+    function showNext() {
+        currentIndex = (currentIndex + 1) % images.length;
+        openLightbox(currentIndex);
+    }
+
+    $('.gallery-item').on('click', function() {
+        openLightbox(images.index(this));
+    });
+
+    $('.close').on('click', function() {
+        closeLightbox();
+    });
+
+    $('.prev').on('click', function() {
+        showPrev();
+    });
+
+    $('.next').on('click', function() {
+        showNext();
+    });
+
+    $(document).on('keydown', function(event) {
+        if (event.key === 'Escape') {
+            closeLightbox();
+        } else if (event.key === 'ArrowLeft') {
+            showPrev();
+        } else if (event.key === 'ArrowRight') {
+            showNext();
+        }
+    });
+});
